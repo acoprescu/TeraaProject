2d6ccb049774c6c0db4873f37fd7de4881a4163b
ocosmin <ocosmin@gmail.com>
2022-04-19 13:58:01 +0300

    Rename MainActivity->IntroActivity

--- app/src/main/java/eu/fastapps/teraaproject/IntroActivity.java
+++ app/src/main/java/eu/fastapps/teraaproject/IntroActivity.java
@@ -5,11 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
 
     Button gameButton;
@@ -24,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
 
         gameButton = findViewById(R.id.button_game);
         gameButton.setOnClickListener(
