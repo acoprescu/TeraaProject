da55a99ecc704ad07f5bfafcd525470256bc081a
ocosmin <ocosmin@gmail.com>
2022-04-20 12:14:49 +0300

    Add WebIntent

--- app/src/main/java/eu/fastapps/teraaproject/InfoActivity.java
+++ app/src/main/java/eu/fastapps/teraaproject/InfoActivity.java
@@ -2,13 +2,25 @@ package eu.fastapps.teraaproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class InfoActivity extends AppCompatActivity {
+
+    public static final String YT_PAGE = "saEpkcVi1d4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        findViewById(R.id.imageYoutube).setOnClickListener(
+                (v)->{
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.youtube.com/watch?v=" + YT_PAGE));
+                    startActivity(webIntent);
+                }
+        );
     }
 }
