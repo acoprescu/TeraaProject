ac74120781d2e2ba618bb535a5453ea648b6656a
ocosmin <ocosmin@gmail.com>
2022-04-21 08:51:55 +0300

    save user details in Setting Screen

--- app/src/main/java/eu/fastapps/teraaproject/SettingsActivity.java
+++ app/src/main/java/eu/fastapps/teraaproject/SettingsActivity.java
@@ -3,12 +3,22 @@ package eu.fastapps.teraaproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Button save;
+    EditText name, email, password;
+    SharedPreferences sp;
+    String nameStr, emailStr, passStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +85,41 @@ public class SettingsActivity extends AppCompatActivity {
         findViewById(R.id.contactus).setOnClickListener(
                 (v) -> startActivity(new Intent(this, ContactActivity.class))
         );
+
+        name = findViewById(R.id.editTextTextPersonName);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        save = findViewById(R.id.button_savee);
+
+        sp = getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
+
+        String nameStr = sp.getString("name", "");
+        name.setText(nameStr);
+
+        String emailStr = sp.getString("email", "");
+        email.setText(emailStr);
+
+        String passStr = sp.getString("password", "");
+        password.setText(passStr);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameStr = name.getText().toString();
+                String emailStr = email.getText().toString();
+                String passStr = password.getText().toString();
+
+                SharedPreferences.Editor editor = sp.edit();
+
+                editor.putString("name", nameStr);
+                editor.putString("email", emailStr);
+                editor.putString("password", passStr);
+                editor.commit();
+                Toast.makeText(SettingsActivity.this, "Information Saved", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
 
     }
 }
