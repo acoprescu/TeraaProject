8786742573afa9095ee56598d3af595e925c1fc8
ocosmin <ocosmin@gmail.com>
2022-04-22 12:11:29 +0300

    Show Info details in WebView

--- app/src/main/java/eu/fastapps/teraaproject/InfoDetailsActivity.java
+++ app/src/main/java/eu/fastapps/teraaproject/InfoDetailsActivity.java
@@ -3,6 +3,8 @@ package eu.fastapps.teraaproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class InfoDetailsActivity extends AppCompatActivity {
@@ -18,7 +20,28 @@ public class InfoDetailsActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.title)).setText(options[position]);
 
         String[] details = getResources().getStringArray(R.array.info_details);
-        ((TextView)findViewById(R.id.content)).setText(details[position]);
+
+        WebView webView = findViewById(R.id.content);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.setWebViewClient(new Callback());
+
+        String data = "<!DOCTYPE> <html><body> $$$ <body><html>"
+                .replace("$$$",details[position]);
+        //System.out.println("!!! " + data);
+        webView.loadDataWithBaseURL(null, data , "text/html", "utf-8", null);
+
+    }
+
+    private class Callback extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
 
     }
 }
